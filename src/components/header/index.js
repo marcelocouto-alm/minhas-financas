fd3540172e18762cc496d9e009daf52b50a4a54a
marcelocouto.alm <marcelocouto.alm@gmail.com>
2023-02-27 18:10:50 -0300

    first commit

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as C from './styles'
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Minhas Finanças</C.Title>
+      </C.Header>
+    </C.Container>
+  );
+};
+
+export default Header;
